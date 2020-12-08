b6c14414712213c54fcc62bd6a7ffbe9abda1260
Rafael Mendes <mendesrafa.c@gmail.com>
2020-12-07 22:52:37 -0300

    adding router and creating main home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyle from "./styles/global";
+import Home from "./pages/Home/index";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <h1>Camps Party</h1>
-    </>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
